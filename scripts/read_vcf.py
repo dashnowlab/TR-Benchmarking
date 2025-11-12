fc9a07331c6ba9c842442468a6ef2fc124774733
Harriet Dashnow <h.dashnow@gmail.com>
2025-11-12 12:19:02 -0700

    added some comments

--- scripts/read_vcf.py
+++ scripts/read_vcf.py
@@ -22,12 +22,15 @@ def main(vcf: str, output: str):
             end_pos = pos + len(record.REF) - 1
             ref_len = len(record.REF)
 
+            # Get all alleles sequences as a list e.g. [REF, ALT1, ALT2]
+            # These can then be indexed by genotype indices e.g. 0 for REF, 1 for ALT1, etc.
             alleles = [record.REF, *record.ALT]
-        
+
+            # Iterate over samples in case of multi-sample VCF
             for sample_idx in range(len(vcf_reader.samples)):
-                #gt = record.gt_bases[sample_idx]
+                # gt = record.gt_bases[sample_idx] # alternative way to get genotype bases
                 gt_idx = record.genotypes[sample_idx]
-                gt = [alleles[gt_idx[0]], alleles[gt_idx[1]]]
+                gt = [alleles[gt_idx[0]], alleles[gt_idx[1]]] # get allele sequences as a list (typically two per sample for autosomal diploid)
                 gt_len = [len(x) for x in gt]
                 outfile.write(f"{chrom}\t{pos-1}\t{end_pos}\t{ref_len}\t{vcf_reader.samples[sample_idx]}\t{'\t'.join(gt)}\t{'\t'.join(map(str, gt_len))}\n")
 
